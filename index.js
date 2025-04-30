692bc960393e41b00cf75251505c3c73b3692b6e
Fearless-Badger <micah.najacht@gmail.com>
2025-04-30 01:14:25 -0500

    Progress in bug fixing

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Calculator Logic
+let operator_present = false;
+let operator = "";
+let expression = "";
+const OPERATORS = ["+", "-", "*", "/"];
+let ans = "";
+let result_displayed = false;
+
 // Display DOM variables
 const DISPLAY_ELEMENT = document.querySelector(".display");
 let display = document.querySelector(".display").textContent;
@@ -9,9 +17,7 @@ NUMBER_BUTTONS.slice(0, NUMBER_BUTTONS.length - 1).map((element) => {
   element.addEventListener("click", () => add_to_display(element.textContent));
 });
 
-NUMBER_BUTTONS.slice(-1)[0].addEventListener("click", () =>
-  operate()
-);
+NUMBER_BUTTONS.slice(-1)[0].addEventListener("click", () => operate());
 
 // Operator listeners
 
@@ -36,20 +42,13 @@ const CLEAR = CONTROLLER_ARRAY[1];
 DELETE.addEventListener("click", () => display_delete());
 CLEAR.addEventListener("click", () => clear_memory());
 
-// Calculator Logic
-let operator_present = false;
-let operator = "";
-let expression = "";
-const OPERATORS = ["+", "-", "*", "/"];
-let ans = "";
-
 function clear_display() {
   display = "";
   DISPLAY_ELEMENT.textContent = "";
 }
 
 function display_delete() {
-  if (display) {
+  if (display && result_displayed===false) {
     display = display.slice(0, display.length - 1);
     expression = expression.slice(0, expression.length - 1);
     DISPLAY_ELEMENT.textContent = display;
@@ -57,6 +56,9 @@ function display_delete() {
 }
 
 function clear_memory() {
+  result_displayed = false;
+  operator="";
+  ans ='';
   clear_display();
   expression = "";
   operator_present = false;
@@ -67,46 +69,51 @@ function help(answer) {
   expression = answer;
   ans = answer;
   operator = "";
+  result_displayed = true;
 }
 
 function operate() {
   clear_display();
   //console.log(`Evaluate : ${expression}`);
 
-  let nums;
-  let answer;
-
-  if (!operator) {
-    help(expression);
-  }
-
-  switch (operator) {
-    case "+":
-      nums = expression.split("+");
-      answer = add(nums[0], nums[1]);
-      help(answer);
-      break;
-    case "-":
-      nums = expression.split("-");
-      answer = subtract(nums[0], nums[1]);
-      help(answer);
-      break;
-    case "*":
-      nums = expression.split("*");
-      answer = multiply(nums[0], nums[1]);
-      help(answer);
-      break;
-    case "/":
-      nums = expression.split("/");
-      answer = divide(nums[0], nums[1]);
-      help(answer);
-      break;
+  if (!expression) {
+    DISPLAY_ELEMENT.textContent = "Enter an expression!";
+  } else if (expression && !operator_present) {
+    DISPLAY_ELEMENT.textContent = expression;
+  } else {
+    let nums;
+    let answer;
+    switch (operator) {
+      case "+":
+        nums = expression.split("+");
+        answer = add(nums[0], nums[1]).toFixed(2);
+        help(answer);
+        break;
+      case "-":
+        nums = expression.split("-");
+        answer = subtract(nums[0], nums[1]).toFixed(2);
+        help(answer);
+        break;
+      case "*":
+        nums = expression.split("*");
+        answer = multiply(nums[0], nums[1]).toFixed(2);
+        help(answer);
+        break;
+      case "/":
+        nums = expression.split("/");
+        answer = divide(nums[0], nums[1]).toFixed(2);
+        help(answer);
+        break;
+    }
   }
 
   operator_present = false;
 }
 
 function add_to_display(val) {
+  if (result_displayed){
+    clear_memory();
+  }
   display += val;
   DISPLAY_ELEMENT.textContent = display;
   add_to_expression(val);
@@ -120,20 +127,24 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-async function add_to_expression(val) {
+function add_to_expression(val) {
   //console.log("val = " + val);
   let is_operator = OPERATORS.includes(val);
-  if (operator_present && is_operator) {
+  if (expression === "" && is_operator) {
+    DISPLAY_ELEMENT.textContent = "Enter a number first!";
+  } else if (operator_present && is_operator) {
     operate();
+    operator_present = true;
     operator = val;
-    //operator_present = true;
     expression = ans + operator;
     DISPLAY_ELEMENT.textContent = ans;
   } else if (is_operator) {
     operator = val;
     clear_display();
-    operator_present = !operator_present;
+    operator_present = true;
     expression += val;
+  } else if (operator_present===false && ans==="" && is_operator) {
+    DISPLAY_ELEMENT.textContent = "Enter an operator";
   } else {
     expression += val;
   }
