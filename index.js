05a9c4a2ef62266de5d7476f25d5dd1586a345e0
Fearless-Badger <micah.najacht@gmail.com>
2025-04-29 19:08:40 -0500

    Finish JS fucntions

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log("Hello, calculator");
+
+function add(alpha, beta){
+    return +(alpha) + +(beta);
+}
+
+function subtract(alpha, beta){
+    return +(alpha) - +(beta);
+}
+
+function multiply(alpha, beta){
+    return +(alpha) * +(beta);
+}
+
+function divide(alpha, beta){
+    if (+beta === 0){
+        return "Blackhole";
+    }
+    return +(alpha) / +(beta);
+}
